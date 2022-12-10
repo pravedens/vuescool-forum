a4d8a7a0eb2508e344dbe6f4d778c6d5fe512d32
praveden <pravedens@gmail.com>
2022-12-10 18:27:49 +0500

    Создание PostEditor

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -4,16 +4,20 @@
 
     <post-list :posts="threadPosts" />
 
+    <post-editor @save="addPost"/>
+
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
 import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
 export default {
   name: 'ThreadShow',
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
   props: {
     id: {
@@ -33,6 +37,17 @@ export default {
     },
     threadPosts () {
       return this.posts.filter(post => post.threadId === this.id)
+    }
+  },
+  methods: {
+    addPost (eventData) {
+      const post = {
+        ...eventData.post,
+        threadId: this.id
+      }
+
+      this.posts.push(post)
+      this.thread.posts.push(post.id)
     }
   }
 }
