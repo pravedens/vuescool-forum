acd603a0766b2f3062e72abba12be2e888d939ec
praveden <pravedens@gmail.com>
2022-12-22 18:05:03 +0500

    Установка и настройка Firebase 8

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyAl14J-72mrZrt6xIPNMdbTy6YrYGLjg60',
+  authDomain: 'vue-school-forum-1eac3.firebaseapp.com',
+  projectId: 'vue-school-forum-1eac3',
+  storageBucket: 'vue-school-forum-1eac3.appspot.com',
+  messagingSenderId: '173267239307',
+  appId: '1:173267239307:web:13912d0fd8f88c5037ef8e'
+}
