573c60a58ff9f3b13f9e6f32ff0dee23808868a4
praveden <pravedens@gmail.com>
2022-12-24 16:20:43 +0500

    Миграция Vuex для использования базы данных Cloud Firestore

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { createStore } from 'vuex'
-import sourceData from '@/data'
 import { findById, upset } from '@/helpers'
 
 export default createStore({
   state: {
-    ...sourceData,
+    categories: [],
+    forums: [],
+    threads: [],
+    posts: [],
+    users: [],
     authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3'
   },
   getters: {
@@ -90,9 +93,8 @@ export default createStore({
     setThread (state, { thread }) {
       upset(state.threads, thread)
     },
-    setUser (state, { user, userId }) {
-      const userIndex = state.users.findIndex(user => user.id === userId)
-      state.users[userIndex] = user
+    setUser (state, { user }) {
+      upset(state.users, user)
     },
     appendPostToThread: makeAppendChildToParentMutation({ parent: 'threads', child: 'posts' }),
     appendThreadToForum: makeAppendChildToParentMutation({ parent: 'forums', child: 'threads' }),
